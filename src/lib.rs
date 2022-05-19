cd13d2fcf552002c017695b336c58639cce2c705
Trevor Gross <tmgross@umich.edu>
2022-05-19 06:26:18 -0400

    Correct links

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 //!
 //! A library for applying text- and token- based comparison algorithms to
 //! determine the similarity of two strings or sets. The core modules are
-//! ['algorithms'], which contains algorithms for determining closeness, and
-//! ['collectors'] which has helper functions for preparing anything for
+//! [`algorithms`], which contains algorithms for determining closeness, and
+//! [`collectors`] which has helper functions for preparing anything for
 //! tokenization and comparison.
 //!
 //! Note that this module is very much a work in progress, and it is likely that
