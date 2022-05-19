86015af77963aea3686e25b711bd51b5c437066b
Trevor Gross <tmgross@umich.edu>
2022-05-19 05:52:45 -0400

    Update docs to hopefully force update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 //! # Stringmetrics library
+//!
 //! A library for applying text- and token- based comparison algorithms to
 //! determine the similarity of two strings or sets. The core modules are
 //! ['algorithms'], which contains algorithms for determining closeness, and
@@ -7,6 +8,8 @@
 //!
 //! Note that this module is very much a work in progress, and it is likely that
 //! interfaces may change. Large parts of this module are still under
-//! construction.
+//! construction. The eventual goal is to implement many of the metrics listed
+//! on [this wikipedia page](https://en.wikipedia.org/wiki/String_metric),
+//! generalized to any hashable datatype.
 
 pub mod algorithms;
