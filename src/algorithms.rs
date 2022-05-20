ea91d10ccf1ba24301d8f6f14b80e4bfdf8e2381
Trevor Gross <tmgross@umich.edu>
2022-05-20 01:36:02 -0400

    Ensured levenshtein_limit_weight is working properly; added docs and tests

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,6 +1,102 @@
 //! This module contains functions for applying various closeness algorithms.
 //!
-//! See the indiviual functions for usage and examples.
+//! See the indiviual functions for usage and examples. This page intends to
+//! give a high level overview of how the functions are implemented.
+//!
+//! # Levenshtein distance algorithm
+//!
+//! The funcition [`levenshtein`] implements the algorithm below:
+//!
+//! $$ \operatorname{lev}_{a,b}(i,j) = \begin{cases} \max(i,j) &\text{if }
+//! \min(i,j) = 0, \\
+//!    \min \begin{cases} \operatorname{lev}_{a,b}(i-1,j) + 1 \\
+//!         \operatorname{lev}_{a,b}(i,j-1) + 1 \\
+//!         \operatorname{lev}_{a,b}(i-1,j-1) + 1_{(a_i \ne b_i)}
+//!       \end{cases}
+//!    &\text{otherwise} \end{cases}$$
+//!
+//! _(erm... I can't seem to get KaTeX working. Let me know on GitHub if you can
+//! help!)_
+//!
+//! Easier shown than read. Basically, the algorithm parses from top left to
+//! bottom right to create a table like follows for the classic example:
+//!
+//! ```text
+//!      j → 0  1  2  3  4  5  6  7
+//! i             str B ->
+//! ↓           s  i  t  t  i  n  g
+//! 0  s    [0, 1, 2, 3, 4, 5, 6, 7]
+//! 1  t  k [1, 1, 2, 3, 4, 5, 6, 7]
+//! 2  r  i [2, 2, 1, 2, 3, 4, 5, 6]
+//! 3     t [3, 3, 2, 1, 2, 3, 4, 5]
+//! 4  A  t [4, 4, 3, 2, 1, 2, 3, 4]
+//! 5  ↓  e [5, 5, 4, 3, 2, 2, 3, 4]
+//! 6     n [6, 6, 5, 4, 3, 3, 2, 3]
+//! ```
+//!
+//! The outer rows (at i=0 and j=0) are just incrementing and can be filled in
+//! automatically. Then, the algorithm works its way left-to-right then
+//! top-to-bottom to fill in the table. Rules are:
+//!
+//! 1. Insertion cost is the value of the field to the left plus one
+//! 2. Deletion cost is the value of the field above plus one
+//! 3. Substitution cost is the value of the field left above if the two
+//!    relevant characters are the same. If they are different, add one to this
+//!    value.
+//! 4. Take the minimum of these three values and put it in the current box.
+//!
+//! Eventually, the above matrix can be filled out and the final "distance" is
+//! the number at the bottom right; in this case, 3.
+//!
+//! This library uses [an algorithm published by Sten
+//! Hjelmqvist](https://www.codeproject.com/Articles/13525/Fast-memory-efficient-Levenshtein-algorithm-2)
+//! and described on [the Levenshtein distance Wikipedia
+//! page](https://en.wikipedia.org/wiki/Levenshtein_distance#Iterative_with_two_matrix_rows)
+//! that only uses two vectors at a time, rather than constructing the entire
+//! matrix, for memory optimizations. Memory use is only that for a vector of
+//! u32 twice the length of string B.
+//! 
+//! ## Limited Levenshtein algorithm
+//! 
+//! This is easy; same algorithm as above, just stop matching when you hit a
+//! desired limit to avoid spending resources on obviously different strings.
+//! Use this version where possible, implemented by [`levenshtein_limit`].
+//! 
+//! ```
+//! use stringmetrics::algorithms::levenshtein_limit;
+//! assert_eq!(levenshtein_limit("superlongstring", "", 3), 3);
+//! ```
+//!
+//! ## Weighted Levenshtein algorithm
+//!
+//! Implemented by [`levenshtein_weight`] and [`levenshtein_limit_weight`], a
+//! weighted Levenshtein algorithm just allows applying differing penalties for
+//! insertion, deletion, and substitution. It's basically the same algorithm as
+//! above, except the added values are weights rather than one, and the initial
+//! row population is related to insertion and deletion weights.
+//! 
+//! For example, the following code:
+//! 
+//! ```
+//! use stringmetrics::algorithms::levenshtein_weight;
+//! assert_eq!(levenshtein_weight("kitten", "sitting", 4, 3, 2), 8);
+//! ```
+//! Creates the following matrix:
+//! 
+//! ```text
+//!       j → 0   1   2   3   4   5   6   7
+//! i             str B ->
+//! ↓             s   i   t   t   i   n   g
+//! 0  s    [ 0,  4,  8, 12, 16, 20, 24, 28]
+//! 1  t  k [ 3,  2,  6, 10, 14, 18, 22, 26]
+//! 2  r  i [ 6,  5,  2,  6, 10, 14, 18, 22]
+//! 3     t [ 9,  8,  5,  2,  6, 10, 14, 18]
+//! 4  A  t [12, 11,  8,  5,  2,  6, 10, 14]
+//! 5  ↓  e [15, 14, 11,  8,  5,  4,  8, 12]
+//! 6     n [18, 17, 14, 11,  8,  7,  4,  8]
+//! ```
+//! 
+//! 
 
 mod basic;
 mod damerau;
--- src/algorithms/levenshtein.rs
+++ src/algorithms/levenshtein.rs
@@ -21,6 +21,7 @@ pub fn levenshtein_limit_weight(
     let a_len = a.len() as u32;
     let b_len = b.len() as u32;
 
+    // Start with some shortcut solution optimizations
     if a_len == 0 {
         return min(b_len * w_ins, limit);
     }
@@ -29,28 +30,31 @@ pub fn levenshtein_limit_weight(
     }
 
     let diff = max(a_len, b_len) - min(a_len, b_len);
-
     if diff >= limit {
         return limit;
     }
 
-    // Create two working vectors
-    let v_len = a.len() + 1;
-    let mut v_prev: Vec<u32> = (0..(v_len as u32)).collect();
-    let mut v_curr: Vec<u32> = vec![0; v_len];
+    // These vectors will hold the "previous" and "active" distance row, rather
+    // than needing to construct the entire array. We want to keep these small
+    // so a vector of u32 is preferred over usize. u16 would be even better but
+    // for long text, that could be hit somewhat easily.
+    let v_len = b_len + 1;
+    let mut v_prev: Vec<u32> = (0..(v_len * w_ins)).step_by(w_ins as usize).collect();
+    let mut v_curr: Vec<u32> = vec![0; v_len as usize];
 
     let mut ins_cost: u32;
     let mut del_cost: u32;
     let mut sub_cost: u32;
+    let mut current_max:u32=0;
 
-    for (i, a_char) in b.chars().enumerate() {
-        v_curr[0] = (i + 1) as u32;
-
+    println!("{:?}",v_prev);
+    // i holds our "vertical" position, j our "horizontal". We fill the table
+    // top to bottom. Note there is actually an offset of 1 from i to the "true"
+    // array position (since we start one row down).
+    for (i, a_char) in a.chars().enumerate() {
+        v_curr[0] = ((i + 1) * w_del as usize) as u32;
         // Fill out the rest of the row
-        for (j, b_char) in a.chars().enumerate() {
-            // println!("{:?}", v_prev);
-            // println!("{:?}", v_curr);
-            // calculating costs for A[i+1][j+1]
+        for (j, b_char) in b.chars().enumerate() {
             ins_cost = v_curr[j] + w_ins;
             del_cost = v_prev[j + 1] + w_del;
             sub_cost = match a_char == b_char {
@@ -58,18 +62,11 @@ pub fn levenshtein_limit_weight(
                 false => v_prev[j] + w_sub,
             };
 
-            v_curr[j + 1]=min(min(ins_cost,del_cost),sub_cost);
-
-            // Figure out which operation we use first, then multiply by weight
-            // if insertion_cost < min(deletion_cost, substitution_cost) {
-            //     v_curr[j + 1] = insertion_cost+ ins_cost-1;
-            // } else if deletion_cost < min(insertion_cost, substitution_cost) {
-            //     v_curr[j + 1] = deletion_cost + del_cost-1;
-            // } else {
-            //     v_curr[j + 1] = substitution_cost + sub_cost-1;
-            // }
+            v_curr[j + 1] = min(min(ins_cost, del_cost), sub_cost);
         }
-        let current_max = v_curr.last().copied().unwrap_or_default();
+        println!("{:?}",v_curr);
+        current_max =  *v_curr.last().unwrap();
+        
         if current_max >= limit {
             return limit;
         }
@@ -79,8 +76,7 @@ pub fn levenshtein_limit_weight(
         std::mem::swap(&mut v_prev, &mut v_curr);
     }
 
-    // Remember we swapped
-    v_prev.last().copied().unwrap_or_default()
+    current_max
 }
 
 /// Levenshtein distance computation with weights
@@ -117,12 +113,7 @@ pub fn levenshtein_weight(a: &str, b: &str, ins_cost: u32, del_cost: u32, sub_co
 /// let b = "mmmmmmm";
 /// assert_eq!(levenshtein_limit(a, b, 3), 3);
 /// ```
-/// 
-/// ````math
-/// f(x) = \int_{-\infty}^\infty
-/// \hat f(\xi)\,e^{2 \pi i \xi x}
-/// \,d\xi
-/// ```
+///
 #[inline]
 pub fn levenshtein_limit(a: &str, b: &str, limit: u32) -> u32 {
     levenshtein_limit_weight(a, b, limit, 1, 1, 1)
@@ -136,7 +127,8 @@ pub fn levenshtein_limit(a: &str, b: &str, limit: u32) -> u32 {
 /// different" (e.g., strings of different lengths). In many cases it is better
 /// to use [`levenshtein_limit`] to avoid unnecessary computation.
 ///
-/// Behind the scenes, this wraps [`levenshtein_limit_weight`].
+/// Behind the scenes, this wraps [`levenshtein_limit_weight`]. For details on
+/// operation, see the [`algorithms`] page.
 ///
 /// # Example
 ///
@@ -146,26 +138,10 @@ pub fn levenshtein_limit(a: &str, b: &str, limit: u32) -> u32 {
 /// let b = "i am a cook";
 /// assert_eq!(levenshtein(a, b), 6);
 /// ```
-/// 
-/// # Details
-/// 
-/// This implements the levenshtein algorithm as defined below:
-/// 
-/// $$
-/// \operatorname{lev}_{a,b}(i,j) = \begin{cases}
-///    \max(i,j) &\text{if } \min(i,j) = 0, \\
-///    \min
-///       \begin{cases}
-///         \operatorname{lev}_{a,b}(i-1,j) + 1 \\
-///         \operatorname{lev}_{a,b}(i,j-1) + 1 \\
-///         \operatorname{lev}_{a,b}(i-1,j-1) + 1_{(a_i \ne b_i)}
-///       \end{cases}
-///    &\text{otherwise} 
-/// \end{cases}
-/// $$
-/// 
+///
 /// Note that sometimes the levenshtein distance is defined as having a default
-/// weight of 2 for substitutions. 
+/// weight of 2 for substitutions. That isn't the case for this implementation -
+/// if you need that functionality, please use [`levenshtein_weight`].
 #[inline]
 pub fn levenshtein(a: &str, b: &str) -> u32 {
     levenshtein_limit_weight(a, b, u32::MAX, 1, 1, 1)
@@ -225,28 +201,27 @@ mod tests {
         assert_eq!(levenshtein_weight("a", "", 10, 1, 1), 1);
         assert_eq!(levenshtein_weight("", "ab", 10, 1, 1), 20);
         assert_eq!(levenshtein_weight("ab", "", 10, 1, 1), 2);
-        assert_eq!(levenshtein_weight("ab", "abcd", 2, 5, 5), 4);
+        assert_eq!(levenshtein_weight("ab", "abcd", 10, 1, 1), 20);
+        assert_eq!(levenshtein_weight("kitten", "sitting", 10, 1, 1), 12);
     }
 
-    // #[test]
-    // fn test_levenshtein_weight_deletion() {
-    //     // assert_eq!(levenshtein_weight("", "a", 1, 10, 1), 1);
-    //     // assert_eq!(levenshtein_weight("a", "", 1, 10, 1), 10);
-    //     // assert_eq!(levenshtein_weight("", "ab", 1, 10, 1), 2);
-    //     // assert_eq!(levenshtein_weight("ab", "", 1, 10, 1), 20);
-    //     assert_eq!(levenshtein_weight("abc", "ac", 5, 10, 2), 10);
-    // }
+    #[test]
+    fn test_levenshtein_weight_deletion() {
+        assert_eq!(levenshtein_weight("", "a", 1, 10, 1), 1);
+        assert_eq!(levenshtein_weight("a", "", 1, 10, 1), 10);
+        assert_eq!(levenshtein_weight("", "ab", 1, 10, 1), 2);
+        assert_eq!(levenshtein_weight("ab", "", 1, 10, 1), 20);
+        assert_eq!(levenshtein_weight("abc", "ac", 1, 10, 2), 10);
+        assert_eq!(levenshtein_weight("abcd", "ac", 1, 10, 2), 20);
+        assert_eq!(levenshtein_weight("kitten", "sitting", 1, 10, 1), 3);
+    }
 
-    // #[test]
-    // fn test_levenshtein_weight_substitution() {
-    //     // Note that when substitution cost is high, the algorithm will prefer
-    //     // a deletion and insertion
-    //     assert_eq!(levenshtein_weight("a", "b", 10, 10, 1), 1);
-    //     assert_eq!(levenshtein_weight("abc", "acc", 10, 10, 2), 2);
-    // }
-
-    // #[test]
-    // fn test_levenshtein_weight_substitution() {
-    //     assert_eq!(levenshtein_weight("kitten", "sitten", 10, 10, 2), 2);
-    // }
+    #[test]
+    fn test_levenshtein_weight_substitution() {
+        // Note that when substitution cost is high, the algorithm will prefer
+        // a deletion and insertion
+        assert_eq!(levenshtein_weight("a", "b", 10, 10, 5), 5);
+        assert_eq!(levenshtein_weight("abcd", "acc", 10, 10, 2), 12);
+        assert_eq!(levenshtein_weight("kitten", "sitting", 4, 3, 2), 8);
+    }
 }
