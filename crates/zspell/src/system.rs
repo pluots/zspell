98179e0102a3073e2884a9eda8ab8dad359e3630
Trevor Gross <tmgross@umich.edu>
2022-08-26 07:01:27 -0400

    Small test clippy fixes

--- crates/zspell/src/system.rs
+++ crates/zspell/src/system.rs
@@ -429,7 +429,7 @@ mod tests {
 
         let mut input = vec![
             dir.path().join("aaa").join("bbb*").join("ccc"),
-            dir.path().join("ddd").to_owned(),
+            dir.path().join("ddd").clone(),
         ];
 
         let mut expanded = Vec::from_iter(expand_dir_wildcards(&mut input));
